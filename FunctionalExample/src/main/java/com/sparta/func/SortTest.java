075f6c374adbcb4fe172d60c779503164d8d7a1c
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-07-14 11:40:15 +0100

    Update SortTest.java

--- FunctionalExample/src/main/java/com/sparta/func/SortTest.java
+++ FunctionalExample/src/main/java/com/sparta/func/SortTest.java
@@ -2,19 +2,46 @@ package com.sparta.func;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortTest {
+//    private static class AuthorComparator implements Comparator<Author> {
+//        @Override
+//        public int compare(Author o1, Author o2) {
+//            return o1.getFirstName().compareTo(o2.getFirstName());
+//        }
+//    }
+
     public static void main(String[] args) {
         List<Author> authorList = getAuthorList();
         List<String> nameList = getNameList(authorList);
+//        Collections.sort(authorList); // why is this an error...
+        // anonymous inner class version
+//        Collections.sort(authorList, new Comparator<Author>() {
+//            public int compare(Author o1, Author o2) {
+//                return o1.getFirstName().compareTo(o2.getFirstName());
+//            }
+//        });
+        Collections.sort(authorList,
+//            Comparator.comparing(Author::getLastName) // using method reference
+            (o1, o2) -> {
+//                System.out.println("Hello!" + nameList); // can have other statements
+                // can access variables from enclosing scope
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        );
         System.out.println(authorList);
+        Collections.sort(nameList); // ...when this is fine?
         System.out.println(nameList);
+        // get a random name to answer this question
+        Collections.shuffle(nameList);
+        System.out.println(nameList.get(0));
     }
 
     private static List<String> getNameList(List<Author> authorList) {
         List<String> result = new ArrayList<>();
-        for(Author author: authorList){
+        for (Author author : authorList) {
             result.add(author.toString());
         }
         return result;
