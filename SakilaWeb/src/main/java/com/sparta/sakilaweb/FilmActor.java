3ed4c0376df5a834536d37d38887114703d537af
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-08-02 15:49:41 +0100

    Created Film

---
+++ SakilaWeb/src/main/java/com/sparta/sakilaweb/FilmActor.java
@@ -0,0 +1,44 @@
+package com.sparta.sakilaweb;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "film_actor")
+public class FilmActor {
+    @EmbeddedId
+    private FilmActorId id;
+
+    @MapsId("filmId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "film_id", nullable = false)
+    private Film film;
+
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    public FilmActorId getId() {
+        return id;
+    }
+
+    public void setId(FilmActorId id) {
+        this.id = id;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+}
